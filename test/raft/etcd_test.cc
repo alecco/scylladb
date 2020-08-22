906e40ec840f03dac8ca7e0c8058ed0a0bf3e216
Alejo Sanchez <alejo.sanchez@scylladb.com>
2020-08-22 20:38:06 +0200

    Proper expected output, update to latest raft
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/raft/etcd_test.cc
+++ test/raft/etcd_test.cc
@@ -6,7 +6,6 @@
 #include "raft/server.hh"
 #include "serializer.hh"
 #include "serializer_impl.hh"
-#include <sstream>
 
 using namespace std::chrono_literals;
 using namespace std::placeholders;
@@ -45,8 +44,10 @@ struct initial_state {
     raft::term_t term = raft::term_t(1);
     raft::server_id vote;
     std::vector<raft::log_entry> log;
+    raft::configuration config;
     state_machine::apply_fn apply;
 };
+
 
 class storage : public raft::storage {
     initial_state _conf;
@@ -59,10 +60,9 @@ public:
         return make_ready_future<std::pair<raft::term_t, raft::server_id>>(term_and_vote);
     }
     virtual future<> store_snapshot(const raft::snapshot& snap, size_t preserve_log_entries) { return make_ready_future<>(); }
-    virtual future<raft::snapshot> load_snapshot() { return make_ready_future<raft::snapshot>(raft::snapshot()); }
     virtual future<> store_log_entries(const std::vector<raft::log_entry_ptr>& entries) { co_return seastar::sleep(1us); };
     virtual future<raft::log> load_log() {
-        raft::log log;
+        raft::log log(raft::snapshot{.config = _conf.config});
         for (auto&& e : _conf.log) {
             log.emplace_back(std::move(e));
         }
@@ -124,14 +124,13 @@ public:
 std::unordered_map<raft::server_id, rpc*> rpc::net;
 
 std::pair<std::unique_ptr<raft::server>, state_machine*>
-create_raft_server(raft::server_id uuid, const raft::configuration& config, initial_state state) {
+create_raft_server(raft::server_id uuid, initial_state state) {
 
     auto sm = std::make_unique<state_machine>(uuid, std::move(state.apply));
     auto& rsm = *sm;
     auto mrpc = std::make_unique<rpc>(uuid);
     auto mstorage = std::make_unique<storage>(state);
     auto raft = std::make_unique<raft::server>(uuid, std::move(mrpc), std::move(sm), std::move(mstorage));
-    raft->set_configuration(config);
 
     return std::make_pair(std::move(raft), &rsm);
 }
@@ -147,7 +146,8 @@ future<std::vector<std::pair<std::unique_ptr<raft::server>, state_machine*>>> cr
 
     for (size_t i = 0; i < states.size(); i++) {
         auto& s = config.servers[i];
-        auto& raft = *rafts.emplace_back(create_raft_server(s.id, config, states[i])).first;
+        states[i].config = config;
+        auto& raft = *rafts.emplace_back(create_raft_server(s.id, states[i])).first;
         co_await raft.start();
     }
 
@@ -159,7 +159,7 @@ struct log_entry {
     int value;
 };
 
-std::vector<raft::log_entry> create_log(const std::initializer_list<log_entry> list) {
+std::vector<raft::log_entry> create_log(std::initializer_list<log_entry> list) {
     std::vector<raft::log_entry> log;
 
     unsigned i = 0;
@@ -220,10 +220,11 @@ struct test_case {
     raft::term_t term;
     const size_t leader;
     const std::vector<std::initializer_list<log_entry>> initial_states;
-    const std::vector<raft::command> updates;
+    // const std::vector<raft::command> updates;
+    const std::vector<int> updates;
     test_case(std::string name, size_t nodes, int term, size_t leader,
             std::vector<std::initializer_list<log_entry>> initial_states,
-            std::vector<raft::command> updates) : name(name),
+            std::vector<int> updates) : name(name),
             nodes(nodes), term(raft::term_t(term)), leader(leader), initial_states(initial_states), updates(updates) {
         assert(leader < nodes && "Leader higher than total nodes");
     }
@@ -232,21 +233,31 @@ struct test_case {
 // Run test case (name, nodes, leader, initial logs, updates)
 future<int> run_test(test_case test) {
 
-    std::vector<initial_state> states(test.nodes);
+    std::vector<initial_state> states(test.nodes);       // Server initial states
+    std::vector<int> expected;                           // Expected output for Raft
+    std::vector<std::vector<int>> committed(test.nodes); // Actual outputs for each server
     states[test.leader].term = test.term;
-// fmt::print("run_test: {} leader {} initial log size {}\n", test.name, test.leader, test.leader < test.initial_states.size()? test.initial_states[test.leader].size() : 0);
-    std::vector<std::vector<int>> committed(test.nodes);
-    size_t expected = test.updates.size();
-if (test.leader < test.initial_states.size()) {
-    expected += test.initial_states[test.leader].size();
-}
+fmt::print("run_test: {} leader {} initial log size {}\n", test.name, test.leader, test.leader < test.initial_states.size()? test.initial_states[test.leader].size() : 0);
+    if (test.leader < test.initial_states.size()) {
+        for (auto log_initializer: test.initial_states[test.leader]) {
+            log_entry le(log_initializer);
+            if (le.term > test.term) {
+                break;
+            }
+            expected.push_back(le.value);
+        }
+    }
+    expected.insert(expected.end(), test.updates.begin(), test.updates.end());
+    size_t expected_entries = expected.size();
+
+    // Server initial logs
     for (size_t i = 0; i < states.size(); ++i) {
         if (i < test.initial_states.size()) {
             states[i].log = create_log(test.initial_states[i]);
         } else {
             states[i].log = {};
         }
-        states[i].apply = std::bind(apply_changes, std::ref(committed[i]), expected, _1, _2, _3);
+        states[i].apply = std::bind(apply_changes, std::ref(committed[i]), expected_entries, _1, _2, _3);
     }
 
     auto rafts = co_await create_cluster(states);
@@ -255,7 +266,8 @@ if (test.leader < test.initial_states.size()) {
     leader.make_me_leader();
 
     // Process all updates in order
-    co_await seastar::parallel_for_each(test.updates, [&] (const update u) {
+    std::vector<raft::command> updates = create_commands<int>(test.updates);
+    co_await seastar::parallel_for_each(updates, [&] (const update u) {
             raft::command cmd = std::get<raft::command>(u);
             tlogger.debug("Adding command entry on leader");
             return leader.add_entry(std::move(cmd), raft::server::wait_type::committed);
@@ -273,15 +285,11 @@ if (test.leader < test.initial_states.size()) {
     }
 
     // Check final state of committed is fine
-    auto leader_committed = committed[test.leader];
 // fmt::print("run_test: {} checking committed\n", test.name);
-// fmt::print("run_test: {} leader {} result {}\n", test.name, test.leader, committed[test.leader]);
     for (size_t i = 0; i < committed.size(); ++i) {
-        if (i != test.leader) {
-fmt::print("run_test: {}        {} result {}\n", test.name, i, committed[i]);
-            if (committed[i] != leader_committed) {
-                co_return 1;
-            }
+fmt::print("run_test: {}  server[{}] {} vs expected {} \n", test.name, i, committed[i], expected);
+        if (committed[i] != expected) {
+            co_return 1;
         }
     }
 
@@ -296,14 +304,11 @@ int main(int argc, char* argv[]) {
 
     std::vector<test_case> replication_tests = {
         // name, servers, current term, leader, initial logs (each), commands to send to leader
-#if 0
-        {"non_empty_leader_log", 2, 1, 1, {{},{{1, 0}, {1, 1}, {1, 3}}}, create_commands<int>({4}) },
-        {"simple1", 1, 1, 0, {{}}, create_commands<int>({1}),},
-        {"simple2", 1, 1, 0, {{{1,10}}}, create_commands<int>({1,2,3}),},
-        {"simple2", 2, 1, 0, {{{1,10}}}, create_commands<int>({1,2,3}),},
-#endif
-        {"simple3", 3, 1, 1, {{{1,10},{1,20},{1,30}}}, create_commands<int>({1,2,3}),},
-        {"simple_many", 3, 1, 1, {{{1,10},{2,20},{3,30}}}, create_commands<int>({1,2,3}),},
+        {"simple_1_1_0_e*_1", 1, 1, 0, {{}}, {1,2},},
+        {"simple_1_1_0_1_2", 1, 1, 0, {{{1,10}}}, {1,2},},
+        {"simple_2_1_0_1_2", 2, 1, 0, {{{1,10}}}, {1,2},},
+        {"simple_2_1_1_1_2", 2, 2, 1, {{{1,10}}}, {1,2},},
+        {"simple_3_2_1_1_2", 3, 2, 1, {{{1,10}}}, {1,2},},
     };
 
     return app.run(argc, argv, [&replication_tests] () -> future<int> {
@@ -313,6 +318,7 @@ int main(int argc, char* argv[]) {
 fmt::print("main: test {} failed\n", test.name);
                 co_return 1; // Fail
             }
+fmt::print("------------------------------------------------------\n");
         }
 fmt::print("main: done\n");
         co_return 0;
