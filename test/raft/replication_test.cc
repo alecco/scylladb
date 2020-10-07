3d587b5c6c5f4c2bcb6cdf09a505a49ed037eba2
Alejo Sanchez <alejo.sanchez@scylladb.com>
2020-10-07 23:46:02 +0200

    raft: log failed test case name
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/raft/replication_test.cc
+++ test/raft/replication_test.cc
@@ -496,6 +496,7 @@ int main(int argc, char* argv[]) {
 
         for (auto test: replication_tests) {
             if (co_await run_test(test) != 0) {
+                tlogger.debug("Test {} failed", test.name);
                 co_return 1; // Fail
             }
         }
