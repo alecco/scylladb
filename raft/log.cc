bb626b2ca636f02d202ce094418654e0293c0dbd
Alejo Sanchez <alejo.sanchez@scylladb.com>
2020-11-06 14:24:16 -0400

    raft: avoid clear() on empty boost::deque (ASAN)
    ASAN throws runtime error:
      /usr/include/boost/container/deque.hpp:1802:63:
                    runtime error: applying non-zero offset 8 to null pointer
    
    Tests: unit ({debug,release,dev})
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- raft/log.cc
+++ raft/log.cc
@@ -80,7 +80,9 @@ void log::truncate_tail(index_t idx) {
     assert(start_idx() <= idx);
 
     if (idx >= last_idx()) {
-        _log.clear();
+        if (!_log.empty()) {     // ASAN runtime error for clear() on empty
+            _log.clear();
+        }
     } else if (idx > start_idx()) {
         _log.erase(_log.begin(), _log.begin() + idx - start_idx() + 1);
     }
