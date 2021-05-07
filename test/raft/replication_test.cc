86e91fb92e238c2775a2a4b2579388a3b8b8b02e
Alejo Sanchez <alejo.sanchez@scylladb.com>
2021-05-07 13:34:45 -0400

    raft: replication test: remove unused param
    elect_new_leader doesn't need to know configuration anymore.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/raft/replication_test.cc
+++ test/raft/replication_test.cc
@@ -561,8 +561,7 @@ void elapse_elections(std::vector<test_server>& rafts) {
 }
 
 future<size_t> elect_new_leader(std::vector<test_server>& rafts,
-        lw_shared_ptr<connected> connected, std::unordered_set<size_t>& in_configuration,
-        size_t leader, size_t new_leader) {
+        lw_shared_ptr<connected> connected, size_t leader, size_t new_leader) {
     BOOST_CHECK_MESSAGE(new_leader < rafts.size(),
             format("Wrong next leader value {}", new_leader));
 
@@ -783,8 +782,7 @@ future<> run_test(test_case test, bool prevote, bool packet_drops) {
             unsigned next_leader = std::get<new_leader>(update);
             auto leader_log_idx = rafts[leader].server->log_last_idx();
             co_await rafts[next_leader].server->wait_log_idx(leader_log_idx);
-            leader = co_await elect_new_leader(rafts, connected, in_configuration, leader,
-                    next_leader);
+            leader = co_await elect_new_leader(rafts, connected, leader, next_leader);
         } else if (std::holds_alternative<partition>(update)) {
             co_await wait_log(rafts, connected, in_configuration, leader);
             pause_tickers(tickers);
@@ -812,8 +810,7 @@ future<> run_test(test_case test, bool prevote, bool packet_drops) {
             }
             if (have_new_leader && new_leader.id != leader) {
                 // New leader specified, elect it
-                leader = co_await elect_new_leader(rafts, connected, in_configuration, leader,
-                        new_leader.id);
+                leader = co_await elect_new_leader(rafts, connected, leader, new_leader.id);
             } else if (partition_servers.find(leader) == partition_servers.end() && p.size() > 0) {
                 // Old leader disconnected and not specified new, free election
                 leader = co_await free_election(rafts);
@@ -1257,7 +1254,7 @@ SEASTAR_TEST_CASE(rpc_leader_election) {
         // Elect 2nd node a leader
         constexpr size_t new_leader = 1;
         pause_tickers(tickers);
-        co_await elect_new_leader(rafts, connected, in_configuration, initial_leader, new_leader);
+        co_await elect_new_leader(rafts, connected, initial_leader, new_leader);
         restart_tickers(tickers);
 
         // Check that no attempts to update RPC were made.
@@ -1365,8 +1362,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_snp) {
 
         // Elect B as leader
         pause_tickers(tickers);
-        auto new_leader = co_await elect_new_leader(rafts, connected, in_configuration,
-            initial_leader, 1);
+        auto new_leader = co_await elect_new_leader(rafts, connected, initial_leader, 1);
         restart_tickers(tickers);
 
         // Heal network partition.
@@ -1475,8 +1471,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_log) {
 
         // Elect B as leader
         pause_tickers(tickers);
-        auto new_leader = co_await elect_new_leader(rafts, connected, in_configuration,
-            initial_leader, 1);
+        auto new_leader = co_await elect_new_leader(rafts, connected, initial_leader, 1);
         restart_tickers(tickers);
 
         // Heal network partition.
@@ -1501,8 +1496,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_log) {
 
         // Elect A leader again.
         pause_tickers(tickers);
-        co_await elect_new_leader(rafts, connected, in_configuration,
-            new_leader, initial_leader);
+        co_await elect_new_leader(rafts, connected, new_leader, initial_leader);
         restart_tickers(tickers);
 
         co_await wait_log(rafts, connected, in_configuration, initial_leader);
@@ -1540,8 +1534,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_log) {
 
         // Elect B as leader
         pause_tickers(tickers);
-        new_leader = co_await elect_new_leader(rafts, connected, in_configuration,
-            initial_leader, 1);
+        new_leader = co_await elect_new_leader(rafts, connected, initial_leader, 1);
         restart_tickers(tickers);
 
         // Heal network partition.
