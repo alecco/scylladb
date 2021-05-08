d1be36e8340fadd5b0c16285725fd3dd68cb83c2
Alejo Sanchez <alejo.sanchez@scylladb.com>
2021-05-08 15:10:28 -0400

    raft: replication test: stop server helper
    Helper to stop a server.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/raft/replication_test.cc
+++ test/raft/replication_test.cc
@@ -506,6 +506,7 @@ public:
     // No copy
     raft_cluster(const raft_cluster&) = delete;
     raft_cluster(raft_cluster&&) = default;
+    future<> stop_server(size_t id);
     size_t size() {
         return _servers.size();
     }
@@ -588,6 +589,11 @@ raft_cluster::raft_cluster(std::vector<initial_state> states, state_machine::app
     }
 }
 
+future<> raft_cluster::stop_server(size_t id) {
+    cancel_ticker(id);
+    co_await _servers[id].server->abort();
+}
+
 future<> raft_cluster::start_all() {
     for (auto& r: _servers) {
         co_await r.server->start();
@@ -599,9 +605,8 @@ future<> raft_cluster::start_all() {
 }
 
 future<> raft_cluster::stop_all() {
-    pause_tickers();
-    for (auto& r: _servers) {
-        co_await r.server->abort();
+    for (size_t s = 0; s < _servers.size(); ++s) {
+        co_await stop_server(s);
     }
 }
 
@@ -819,8 +824,7 @@ future<> raft_cluster::change_configuration(size_t total_values, set_config sc) 
     pause_tickers();
     for (auto s: _in_configuration) {
         if (!new_config.contains(s)) {
-            cancel_ticker(s);
-            co_await _servers[s].server->abort();
+            co_await stop_server(s);
             _servers[s] = create_raft_server(to_raft_id(s), _apply, initial_state{.log = {}},
                     total_values, _connected, _snapshots, _persisted_snapshots, _packet_drops);
             co_await _servers[s].server->start();
@@ -1513,7 +1517,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_log) {
         // it's cut off the other nodes and it will be waiting for them,
         // but A is terminated before the network is allowed to heal the partition.
         rafts.cancel_ticker(0);
-        co_await rafts[initial_leader].server->abort();
+        co_await rafts.stop_server(initial_leader);
         // Restart A with a synthetic initial state that contains two entries
         // in the log:
         //   1. {A, B, C} configuration committed before crash + partition.
