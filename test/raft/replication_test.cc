bfd0ae73b0e84a412c3940ca45d1dce7011daa62
Alejo Sanchez <alejo.sanchez@scylladb.com>
2021-05-08 21:11:29 -0400

    raft: replication test: use disconnect() helper
    For rpc tests, use raft_cluster::disconnect() instead of the local
    connected reference.
    
    This removes connected object use outside raft_cluster.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/raft/replication_test.cc
+++ test/raft/replication_test.cc
@@ -1389,7 +1389,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_snp) {
         const auto all_nodes = full_cluster_address_set(rafts.size());
         pause_tickers(tickers);
         // Disconnect A from B and C.
-        connected->disconnect(to_raft_id(0));
+        rafts.disconnect(0);
         // Emulate a failed configuration change on A (add node D) by
         // restarting A with a modified initial log containing one extraneous
         // configuration entry.
@@ -1499,7 +1499,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_log) {
         //
 
         // Disconnect A from the rest of the cluster.
-        connected->disconnect(to_raft_id(0));
+        rafts.disconnect(0);
         // Try to change configuration (remove node C)
         auto uncommitted_conf = address_set({to_raft_id(0), to_raft_id(1)});
         // `set_configuration` call will fail on A because
@@ -1572,7 +1572,7 @@ SEASTAR_TEST_CASE(rpc_configuration_truncate_restore_from_log) {
         co_await wait_log(rafts[new_leader].server, rafts[2].server);
 
         // Disconnect A from the rest of the cluster.
-        connected->disconnect(to_raft_id(0));
+        rafts.disconnect(0);
 
         // Try to add D back.
         tickers[0].cancel();
