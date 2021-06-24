fb144fb07999925eed30aea1744a79b5aecba196
Alejo Sanchez <alejo.sanchez@scylladb.com>
2021-06-24 12:40:00 +0100

    raft: dueling_candidates test
    Copy from replication_test.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- configure.py
+++ configure.py
@@ -553,6 +553,7 @@ perf_tests = set([
 raft_tests = set([
     'test/raft/replication_test',
     'test/raft/randomized_nemesis_test',
+    'test/raft/dueling_test',
     'test/raft/fsm_test',
     'test/raft/etcd_test',
     'test/raft/raft_sys_table_storage_test',
@@ -1255,6 +1256,7 @@ deps['test/boost/alternator_unit_test'] += ['alternator/base64.cc']
 
 deps['test/raft/replication_test'] = ['test/raft/replication_test.cc'] + scylla_minimal_raft_dependencies
 deps['test/raft/randomized_nemesis_test'] = ['test/raft/randomized_nemesis_test.cc'] + scylla_minimal_raft_dependencies
+deps['test/raft/dueling_test'] = ['test/raft/dueling_test.cc'] + scylla_raft_dependencies
 deps['test/raft/fsm_test'] =  ['test/raft/fsm_test.cc', 'test/lib/log.cc'] + scylla_minimal_raft_dependencies
 deps['test/raft/etcd_test'] =  ['test/raft/etcd_test.cc', 'test/lib/log.cc'] + scylla_minimal_raft_dependencies
 deps['test/raft/raft_sys_table_storage_test'] = ['test/raft/raft_sys_table_storage_test.cc'] + \
