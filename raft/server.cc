56458ca6e805194899acc81081473f0effea5434
Konstantin Osipov <kostja@scylladb.com>
2021-07-02 18:02:13 +0300

    raft: (server) implement id() helper
    There is no easy way to get server id otherwise.

--- raft/server.cc
+++ raft/server.cc
@@ -89,6 +89,7 @@ public:
     void elapse_election() override;
     bool is_leader() override;
     void tick() override;
+    raft::server_id id() const override;
     future<> stepdown(logical_clock::duration timeout) override;
 private:
     std::unique_ptr<rpc> _rpc;
@@ -1057,6 +1058,10 @@ void server_impl::elapse_election() {
 
 void server_impl::tick() {
     _fsm->tick();
+}
+
+raft::server_id server_impl::id() const {
+    return _id;
 }
 
 const server_address_set& server_impl::get_rpc_config() const {
