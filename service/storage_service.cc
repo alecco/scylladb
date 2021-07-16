ManManson/scylla

raft: (service) manage Raft configuration during topology changes
Operations of adding or removing a node to Raft configuration
are made idempotent: they do nothing if already done, and
they are safe to resume after a failure.

However, since topology changes are not transactional, if a
bootstrap or removal procedure fails midway, Raft group 0
configuration may go out of sync with topology state as seen by
gossip.

In future we must change gossip to avoid making any persistent
changes to the cluster: all changes to persistent topology state
will be done exclusively through Raft Group 0.

Specifically, instead of persisting the tokens by advertising
them through gossip, the bootstrap will commit a change to a system
table using Raft group 0. nodetool will switch from looking at
gossip-managed tables to consulting with Raft Group 0 configuration.
Once this transformation is done, naturally, adding a node to Raft
configuration will become the first persistent change to ring state
applied when a node joins; removing a node from the Raft Group 0
configuration will become the last action when removing a node.

Until this is done, do our best to avoid a cluster state when
a removed node or a node which addition failed is stuck in Raft
configuration, but the node is no longer present in gossip-managed
system tables. In other words, keep the gossip the primary source of
truth. For this purpose, carefully chose the timing when we
join and leave Raft group 0:

Join the Raft group 0 only after we've advertised our tokens, so the
cluster is aware of this node, it's visible in nodetool status,
but before node state jumps to "normal", i.e. before it accepts
queries. Since the operation is idempotent, invoke it on each
restart.

Remove the node from Group 0 *before* its tokens are removed
from gossip-managed system tables. This guarantees
that if removal from Raft group 0 fails for whatever reason,
the node stays in the ring, so nodetool removenode and
friends are re-tried.

While 

service/storage_service.cc
@@ -48,6 +48,7 @@
 #include "log.hh"
 #include "service/migration_manager.hh"
 #include "service/storage_proxy.hh"
+#include "service/raft/raft_services.hh"
 #include "to_string.hh"
 #include "gms/gossiper.hh"
 #include "gms/failure_detector.hh"
@@ -637,6 +638,15 @@ void storage_service::join_token_ring(int delay) {
 
     // start participating in the ring.
     set_gossip_tokens(_gossiper, _bootstrap_tokens, _cdc_gen_id);
+
+    // Until topology tables are managed by Raft, Gossip continues to be
+    // the primary source of truth, so we should initialize Raft
+    // once gossip tokens have been persisted, so that if
+    // initialization fails, the node is visible to removenode
+    // which can be used to clean this node's state from the
+    // cluster.
+    _raft_svcs.join_raft_group0().get();
+
     set_mode(mode::NORMAL, "node is now in normal status", true);
 
     if (get_token_metadata().sorted_tokens().empty()) {
@@ -724,6 +734,7 @@ void storage_service::bootstrap() {
             slogger.debug("Removing replaced endpoint {} from system.peers", *replace_addr);
             db::system_keyspace::remove_endpoint(*replace_addr).get();
         }
+        _raft_svcs.leave_raft_group0().get();
     }
 
     _db.invoke_on_all([this] (database& db) {
@@ -2350,6 +2361,7 @@ future<> storage_service::removenode(sstring host_id_string, std::list<gms::inet
 
 
                 // Step 5: Announce the node has left
+                ss._raft_svcs.leave_raft_group0().get();
                 ss._gossiper.advertise_token_removed(endpoint, host_id).get();
                 std::unordered_set<token> tmp(tokens.begin(), tokens.end());
                 ss.excise(std::move(tmp), endpoint);
@@ -3654,6 +3666,7 @@ future<> storage_service::force_remove_completion() {
                             slogger.warn("No host_id is found for endpoint {}", endpoint);
                             continue;
                         }
+                        ss._raft_svcs.leave_raft_group0().get();
                         ss._gossiper.advertise_token_removed(endpoint, host_id).get();
                         std::unordered_set<token> tokens_set(tokens.begin(), tokens.end());
                         ss.excise(tokens_set, endpoint);

alecco/scylladb

service/storage_service.cc
@@ -48,6 +48,7 @@
 #include "log.hh"
 #include "service/migration_manager.hh"
 #include "service/storage_proxy.hh"
+#include "service/raft/raft_group_registry.hh"
 #include "to_string.hh"
 #include "gms/gossiper.hh"
 #include "gms/failure_detector.hh"
@@ -638,6 +639,15 @@ void storage_service::join_token_ring(int delay) {
 
     // start participating in the ring.
     set_gossip_tokens(_gossiper, _bootstrap_tokens, _cdc_gen_id);
+
+    // Until topology tables are managed by Raft, Gossip continues to be
+    // the primary source of truth, so we should initialize Raft
+    // once gossip tokens have been persisted, so that if
+    // initialization fails, the node is visible to removenode
+    // which can be used to clean this node's state from the
+    // cluster.
+    _raft_gr.join_raft_group0().get();
+
     set_mode(mode::NORMAL, "node is now in normal status", true);
 
     if (get_token_metadata().sorted_tokens().empty()) {
@@ -737,6 +747,7 @@ void storage_service::bootstrap() {
             slogger.debug("Removing replaced endpoint {} from system.peers", *replace_addr);
             db::system_keyspace::remove_endpoint(*replace_addr).get();
         }
+        _raft_gr.leave_raft_group0().get();
     }
 
     _db.invoke_on_all([this] (database& db) {
@@ -2371,6 +2382,7 @@ future<> storage_service::removenode(sstring host_id_string, std::list<gms::inet
 
 
                 // Step 5: Announce the node has left
+                ss._raft_gr.leave_raft_group0().get();
                 ss._gossiper.advertise_token_removed(endpoint, host_id).get();
                 std::unordered_set<token> tmp(tokens.begin(), tokens.end());
                 ss.excise(std::move(tmp), endpoint);
@@ -3673,6 +3685,7 @@ future<> storage_service::force_remove_completion() {
                             slogger.warn("No host_id is found for endpoint {}", endpoint);
                             continue;
                         }
+                        ss._raft_gr.leave_raft_group0().get();
                         ss._gossiper.advertise_token_removed(endpoint, host_id).get();
                         std::unordered_set<token> tokens_set(tokens.begin(), tokens.end());
                         ss.excise(tokens_set, endpoint);