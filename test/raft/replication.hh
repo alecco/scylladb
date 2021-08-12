1f397a3d50fe13be0eb62f03046c6dab1dbe07b3
Gleb Natapov <gleb@scylladb.com>
2021-08-12 14:46:47 +0300

    raft: make read_barrier work on a follower as well as on a leader
    This patch implements RAFT extension that allows to perform linearisable
    reads by accessing local state machine. The extension is described
    in section 6.4 of the PhD. To sum it up to perform a read barrier on
    a follower it needs to asks a leader the last committed index that it
    knows about. The leader must make sure that it is still a leader before
    answering by communicating with a quorum. When follower gets the index
    back it waits for it to be applied and by that completes read_barrier
    invocation.
    
    The patch adds three new RPC: read_barrier, read_barrier_reply and
    execute_read_barrier_on_leader. The last one is the one a follower uses
    to ask a leader about safe index it can read. First two are used by a
    leader to communicate with a quorum.

--- test/raft/replication.hh
+++ test/raft/replication.hh
@@ -598,6 +598,37 @@ public:
         }
         net[id]->_client->timeout_now_request(_id, std::move(timeout_now));
     }
+    virtual void send_read_quorum(raft::server_id id, const raft::read_quorum& read_quorum) {
+        if (!net.count(id)) {
+            return;
+        }
+        if (!(*_connected)(id, _id)) {
+            return;
+        }
+        if (!drop_packet()) {
+            net[id]->_client->read_quorum_request(_id, read_quorum);
+        }
+    }
+    virtual void send_read_quorum_reply(raft::server_id id, const raft::read_quorum_reply& reply) {
+        if (!net.count(id)) {
+            return;
+        }
+        if (!(*_connected)(id, _id)) {
+            return;
+        }
+        if (!drop_packet()) {
+            net[id]->_client->read_quorum_reply(_id, std::move(reply));
+        }
+    }
+    virtual future<raft::read_barrier_reply> execute_read_barrier_on_leader(raft::server_id id) {
+        if (!net.count(id)) {
+            return make_exception_future<raft::read_barrier_reply>(std::runtime_error("trying to send a message to an unknown node"));
+        }
+        if (!(*_connected)(id, _id)) {
+            return make_exception_future<raft::read_barrier_reply>(std::runtime_error("cannot send append since nodes are disconnected"));
+        }
+        return net[id]->_client->execute_read_barrier(_id);
+    }
     virtual void add_server(raft::server_id id, bytes node_info) {
         _known_peers.insert(raft::server_address{id});
         ++_servers_added;
