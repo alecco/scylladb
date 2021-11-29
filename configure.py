bdd430e67e63713ca4c45c43c1becc46a07fbe0c
Alejo Sanchez <alejo.sanchez@scylladb.com>
2021-11-29 11:10:04 +0100

    raft: temporarily disable many test
    Disable many test until issue with group0 patch is fixed.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- configure.py
+++ configure.py
@@ -558,7 +558,6 @@ perf_tests = set([
 raft_tests = set([
     'test/raft/replication_test',
     'test/raft/randomized_nemesis_test',
-    'test/raft/many_test',
     'test/raft/fsm_test',
     'test/raft/etcd_test',
     'test/raft/raft_sys_table_storage_test',
@@ -1273,7 +1272,6 @@ deps['test/boost/schema_loader_test'] += ['tools/schema_loader.cc']
 
 deps['test/raft/replication_test'] = ['test/raft/replication_test.cc', 'test/raft/replication.cc', 'test/raft/helpers.cc'] + scylla_raft_dependencies
 deps['test/raft/randomized_nemesis_test'] = ['test/raft/randomized_nemesis_test.cc'] + scylla_raft_dependencies
-deps['test/raft/many_test'] = ['test/raft/many_test.cc', 'test/raft/replication.cc', 'test/raft/helpers.cc'] + scylla_raft_dependencies
 deps['test/raft/fsm_test'] =  ['test/raft/fsm_test.cc', 'test/raft/helpers.cc', 'test/lib/log.cc'] + scylla_raft_dependencies
 deps['test/raft/etcd_test'] =  ['test/raft/etcd_test.cc', 'test/raft/helpers.cc', 'test/lib/log.cc'] + scylla_raft_dependencies
 deps['test/raft/raft_sys_table_storage_test'] = ['test/raft/raft_sys_table_storage_test.cc'] + \
