0efa1fda9e00c9c637d920d79870807aecada45e
Kamil Braun <kbraun@scylladb.com>
2021-12-15 15:51:41 +0100

    create table statement use timestamp

--- cql3/statements/create_table_statement.cc
+++ cql3/statements/create_table_statement.cc
@@ -110,7 +110,7 @@ create_table_statement::prepare_schema_mutations(query_processor& qp, api::times
     std::vector<mutation> m;
 
     try {
-        m = co_await qp.get_migration_manager().prepare_new_column_family_announcement(get_cf_meta_data(qp.db()));
+        m = co_await qp.get_migration_manager().prepare_new_column_family_announcement(get_cf_meta_data(qp.db()), ts);
 
         using namespace cql_transport;
         ret = ::make_shared<event::schema_change>(
