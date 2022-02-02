3be1e9d79982690754bf30dad16a3ccd2dfe8f1e
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-02-01 22:45:53 -0400

    test.py topology: check column schema
    Verify column schema for all tables or given table name.
    
    For now it only verifies column type, kind, and "position" (0 for
    pk/clustering and -1 for regular columns).
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/topology/test_schema.py
+++ test/topology/test_schema.py
@@ -37,6 +37,11 @@ async def test_new_table(cql, tables):
 
 
 @pytest.mark.asyncio
-async def test_verify_schema(tables):
+async def test_verify_schema(cql, tables):
     """Verify table schema"""
+    table = tables[0]
     await tables.verify_schema()
+    # Manually remove a column
+    await cql.run_async(f"ALTER TABLE {table.full_name} DROP {table.columns[-1].name}")
+    with pytest.raises(AssertionError, match='Column'):
+        await tables.verify_schema()
