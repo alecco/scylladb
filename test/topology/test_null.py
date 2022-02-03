ed8907671a1fa9e7a2aa360951d25e54f363163e
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-02-03 12:44:27 -0400

    test.py topology: table helper
    Add Table helper classes to handle tables in topology tests.
    TestTables class creates and manages automatically generated Tables for
    a given test.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/topology/test_null.py
+++ test/topology/test_null.py
@@ -4,24 +4,17 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 #
 from cassandra.protocol import InvalidRequest
-from pylib.util import random_string, unique_name
+from pylib.util import random_string
 import pytest
 
 
-@pytest.fixture(scope="session")
-async def table1(cql, keyspace):
-    table = keyspace + "." + unique_name()
-    cql.execute(f"CREATE TABLE {table} (p text, c text, v text, primary key (p, c))")
-    yield table
-    await cql.run_async("DROP TABLE " + table)
-
-
 @pytest.mark.asyncio
-async def test_delete_empty_string_key(cql, table1):
+@pytest.mark.ntables(1)
+async def test_delete_empty_string_key(cql, tables):
     s = random_string()
     # An empty-string clustering *is* allowed:
-    await cql.run_async(f"DELETE FROM {table1} WHERE p='{s}' AND c=''")
+    await cql.run_async(f"DELETE FROM {tables[0].full_name} WHERE pk='{s}' AND c_01=''")
     # But an empty-string partition key is *not* allowed, with a specific
     # error that a "Key may not be empty":
     with pytest.raises(InvalidRequest, match='Key may not be empty'):
-        await cql.run_async(f"DELETE FROM {table1} WHERE p='' AND c='{s}'")
+        await cql.run_async(f"DELETE FROM {tables[0].full_name} WHERE pk='' AND c_01='{s}'")
