cbc9d9517d102a56b5273d71e4364d41b1951108
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-02-16 10:00:41 -0400

    WIP harness

--- test/topology/conftest.py
+++ test/topology/conftest.py
@@ -443,3 +443,74 @@ def cassandra_bug(cql):
     names = [row.table_name for row in cql.execute("SELECT * FROM system_schema.tables WHERE keyspace_name = 'system'")]
     if not any('scylla' in name for name in names):
         pytest.xfail('A known Cassandra bug')
+
+
+class Node:
+    """Node management class for tests"""
+    newid = itertools.count(start=1).__next__
+
+    def __init__(self, name=None):
+        self.id = HarnessCluster.newid()
+        self.name = name if name is not None else f"n_{self.id:02}"
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def restart(self):
+        pass
+
+
+class HarnessCluster:
+    newid = itertools.count(start=1).__next__
+
+    def __init__(self, name=None, nodes=3, dcs=1):
+        self.id = HarnessCluster.newid()
+        self.name = name if name is not None else f"t_{self.id:02}"
+        self.nodes = {}
+        [self.new() for _ in range(nodes)]
+
+    def new(self):
+        """Create a node and return its id"""
+        node = Node()
+        self.nodes[node.id] = node
+        return node.uuid()
+
+    def remove(self, node_id):
+        """Remove node by id"""
+        # XXX stop
+        del self.cql[node_id]
+
+    async def query_node(self, query, node=None):
+        node = random.randint(0, len(self.nodes) - 1) if node is None else node
+        """Remove node by UUID"""
+        if self.cql[node] is None:
+            self.connect_cql(node)
+        return await self.cql[node].run_async(query)
+
+    async def populate(self, query, node=None):
+        node = random.randint(0, len(self.nodes) - 1) if node is None else node
+        """Remove node by UUID"""
+
+
+class Harness:
+    def __init__(self):
+        pass
+
+    async def new_cluster(self, populate=True):
+        cluster = HarnessCluster()
+        if populate:
+            await cluster.populate()
+        return cluster
+
+
+@pytest.fixture(scope="module")
+def harness():
+    return Harness()
+
+
+@pytest.fixture(scope="module")
+def cluster(harness):
+    return harness.new_cluster()
