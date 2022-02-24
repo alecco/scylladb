a107dfd7f20e3a32c64507a3a6618b46b7b7432f
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-02-23 20:50:57 -0400

    (SQUASH) host registry enhacements
    [Proposed HostRegistry implementation improvements]
    
    Use a subdir instead of lose files on /tmp
    Backoff after 10 failed attempts to lock a subnet
    Use itertools safe counter

--- test/pylib/host_registry.py
+++ test/pylib/host_registry.py
@@ -5,9 +5,22 @@
 #
 import errno
 import fcntl
+import itertools
+import logging
 import os
 import random
+import tempfile
+from time import sleep
 from test.pylib.pool import Pool
+
+
+logger = logging.getLogger("host-registry")
+
+# Temporary subdirectory to hold Scylla harness temporary files
+SCYLLA_HARNESS_TMP = "/scylla-py-harness"
+# After threshold attempts to lock a subnet, backoff for delay seconds
+BACKOFF_THRESHOLD = 10
+BACKOFF_DELAY = .5
 
 
 class HostRegistry:
@@ -15,6 +28,7 @@ class HostRegistry:
     which we need to manage and share across many running tests. Store
     all shared external resources within this class to make sure
     nothing is leaked by the harness."""
+    newid = itertools.count(start=1).__next__
 
     def __init__(self):
 
@@ -36,13 +50,15 @@ class HostRegistry:
         # should try again with another subnet. If the file isn't
         # locked, it remains from some previous invocation and can be
         # locked and reused.
+
+        fail = 0
         while True:
             # Avoid 127.0.*.* since CCM (a different test framework)
             # assumes it will be available for it to run Scylla
             # instances. 127.255.255.255 is also illegal.
             self.subnet = "127.{}.{}".format(random.randrange(1, 254),
                                              random.randrange(0, 255))
-            self.lock_filename = os.path.join(os.getenv('TMPDIR', '/tmp'), "scylla-" + self.subnet)
+            self.lock_filename = os.path.join(self.get_tmp_dir(), "scylla-" + self.subnet)
             self.lock_file = open(self.lock_filename, "w")
             try:
                 fcntl.lockf(self.lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -50,6 +66,12 @@ class HostRegistry:
             except OSError as e:
                 if e.errno != errno.EACCESS and e.errno != errno.EAGAIN:
                     raise
+                else:
+                    if fail > BACKOFF_THRESHOLD:
+                        sleep(BACKOFF_DELAY)
+                        fail = 0
+                    else:
+                        fail += 1
             self.lock_file.close()
 
         self.subnet += ".{}"
@@ -66,6 +88,18 @@ class HostRegistry:
                 os.unlink(self.lock_filename)
 
         self.cleanup = cleanup
+
+    def get_tmp_dir(self):
+        """Get a temporary sub-directory for harness test files"""
+        tmp_base = tempfile.gettempdir()  # e.g. /tmp
+        tmp_dir = tmp_base + SCYLLA_HARNESS_TMP
+        while not os.path.isdir(tmp_dir):
+            try:
+                os.mkdir(tmp_dir)
+            except FileExistsError:
+                logger.warn("directory creation race")
+                sleep(1)
+        return tmp_dir
 
     async def lease_host(self):
         return await self.pool.get()
