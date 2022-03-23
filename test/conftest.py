92ed9f6afd314bad9eab0ed280a4a67ea3ca3602
Konstantin Osipov <kostja@scylladb.com>
2022-03-23 15:01:55 +0300

    test.py: use a shared conftest.py from test/ dir

---
+++ test/conftest.py
@@ -0,0 +1,6 @@
+# Copyright 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+# This file configures pytest for all tests in this directory, and also
+# defines common test fixtures for all of them to use.
