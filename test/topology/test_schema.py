2d9a28320e46116bb31c5b1fad984fae5676d54b
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-12 11:45:33 +0200

    test.py: schema helpers
    Helpers to create keyspace and table schema for a given tests.
    
    Drop all tables after test finishes.
    
    Includes helper methods to verify schema consistency.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/topology/test_schema.py
@@ -0,0 +1,50 @@
+# Copyright 2022-present ScyllaDB
+#
+# This file is part of Scylla.
+#
+# Scylla is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Scylla is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+from cassandra.protocol import InvalidRequest                            # type: ignore
+
+
+# Simple test of schema helper
+@pytest.mark.asyncio
+async def test_new_table(cql, tables):
+    table = await tables.add_table(ncolumns=5)
+    await cql.run_async(f"INSERT INTO {table} ({','.join(c.name for c in table.columns)})" +
+                        f"VALUES ({', '.join(['%s'] * len(table.columns))})",
+                        parameters=[c.val(1) for c in table.columns])
+    res = [row for row in await cql.run_async(f"SELECT * FROM {table} "
+                                              f"WHERE {table.columns[0]}='1' "
+                                              f"AND {table.columns[1]}='1'")]
+    assert len(res) == 1
+    assert list(res[0])[:2] == ['1', '1']
+    await tables.drop_table(table)
+    with pytest.raises(InvalidRequest, match='unconfigured table'):
+        await cql.run_async(f"SELECT * FROM {table}")
+    tables.verify_schema()
+
+
+# Simple test of schema helper with alter
+@pytest.mark.asyncio
+async def test_alter_verify_schema(cql, tables):
+    """Verify table schema"""
+    await tables.add_tables(ntables=4, ncolumns=5)
+    await tables.verify_schema()
+    # Manually remove a column
+    table = tables[0]
+    await cql.run_async(f"ALTER TABLE {table} DROP {table.columns[-1].name}")
+    with pytest.raises(AssertionError, match='Column'):
+        await tables.verify_schema()
