718d4493517ce25ee21f6b2758312614738b7f8c
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-12 11:45:33 +0200

    test.py: schema helpers
    Helpers to create keyspace and table schema for a given tests.
    
    Don't bother dropping tables or keyspace by default.
    
    Includes helper methods to verify schema consistency.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/topology/test_null.py
+++ test/topology/test_null.py
@@ -5,23 +5,18 @@
 #
 from cassandra.protocol import InvalidRequest   # type: ignore
 from pylib.util import unique_name
+from pylib.schema_helper import get_schema
 import pytest
 
 
-@pytest.fixture(scope="module")
-async def table1(cql, keyspace):
-    table = keyspace + "." + unique_name()
-    cql.execute(f"CREATE TABLE {table} (p text, c text, v text, primary key (p, c))")
-    yield table
-    await cql.run_async("DROP TABLE " + table)
-
-
 @pytest.mark.asyncio
-async def test_delete_empty_string_key(cql, table1):
+async def test_delete_empty_string_key(cql):
+    tables = await get_schema("delete_empty_string_key", cql, ntables=1, ncolumns=5)
     s = "foobar"
     # An empty-string clustering *is* allowed:
-    await cql.run_async(f"DELETE FROM {table1} WHERE p='{s}' AND c=''")
+    await cql.run_async(f"DELETE FROM {tables[0]} WHERE pk = '{s}' AND {tables[0].columns[1].name} = ''")
     # But an empty-string partition key is *not* allowed, with a specific
     # error that a "Key may not be empty":
     with pytest.raises(InvalidRequest, match='Key may not be empty'):
-        await cql.run_async(f"DELETE FROM {table1} WHERE p='' AND c='{s}'")
+        await cql.run_async(f"DELETE FROM {tables[0]} WHERE pk = '' AND {tables[0].columns[1].name} = '{s}'")
+    await tables.verify_schema()
