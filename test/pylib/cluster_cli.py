0603b5119a2c2c3a522b6e9cde20e6014dc99e25
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-19 16:13:05 +0200

    test.py: harness for cluster management from test
    Harness for managing cluster for Python tests.
    
    Change cluster management from a list of ScyllaServer to Harness.
    Provide tests a REST API with a unix socket for control.
    
    Add simple topology tests with a simple schema change in test_topology.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/pylib/cluster_cli.py
@@ -0,0 +1,60 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+from __future__ import annotations
+import aiohttp.web
+
+
+class ClusterCli():
+    def __init__(self, sock_path: str):
+        self.sock_path = sock_path
+        self.conn = aiohttp.UnixConnector(path=self.sock_path)
+        self.session = aiohttp.ClientSession(connector=self.conn)
+
+    async def stop(self):
+        resp = await self.session.get("http://localhost/cluster/stop")
+        return resp
+
+    async def start(self):
+        resp = await self.session.get("http://localhost/cluster/start")
+        return resp
+
+    async def nodes(self):
+        resp = await self.session.get("http://localhost/cluster/nodes")
+        host_list = await resp.text()
+        return host_list.split(',')
+
+    async def node_stop(self, node_id: str):
+        resp = await self.session.get(f"http://localhost/cluster/node/{node_id}/stop")
+        ret = await resp.text()
+        return ret == "OK"
+
+    async def node_start(self, node_id: str):
+        resp = await self.session.get(f"http://localhost/cluster/node/{node_id}/start")
+        ret = await resp.text()
+        return ret == "OK"
+
+    async def node_restart(self, node_id: str):
+        resp = await self.session.get(f"http://localhost/cluster/node/{node_id}/restart")
+        ret = await resp.text()
+        return ret == "OK"
+
+    async def node_add(self):
+        resp = await self.session.get("http://localhost/cluster/addnode")
+        return await resp.text()
+
+    async def node_remove(self, node_id: str):
+        resp = await self.session.get(f"http://localhost/cluster/removenode/{node_id}")
+        # return await resp.text()
+
+    async def node_decommission(self, node_id: str):
+        resp = await self.session.get(f"http://localhost/cluster/decommission/{node_id}")
+        ret = await resp.text()
+        return ret == "OK"
+
+    async def node_replace(self, node_id: str):
+        resp = await self.session.get(f"http://localhost/cluster/replace/{node_id}")
+        ret = await resp.text()
+        return ret == "OK"
