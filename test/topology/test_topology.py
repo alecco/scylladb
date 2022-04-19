acd5cbdd506d9f9f35cb76f9ff37f79af0f9c846
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-19 16:13:05 +0200

    test.py: harness for cluster management from test
    Harness for managing cluster for Python tests.
    
    Change cluster management from a list of ScyllaServer to Harness.
    Provide tests a REST API with a unix socket for control.
    
    Add simple topology tests with a simple schema change in test_topology.
    
    Clusters can be marked recycled but are not yet recycled in test.py.
    This will come in a later series.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/topology/test_topology.py
@@ -0,0 +1,85 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+"""
+Simple tests for consistency of schema changes while there are topology changes.
+"""
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_1(cluster_api, random_tables):
+    await cluster_api.node_add()
+    nodes = await cluster_api.nodes()
+    print(f"XXX test_1 {nodes}")
+    raise Exception("XXX")
+
+@pytest.mark.asyncio
+async def test_2(cluster_api, random_tables):
+    await cluster_api.node_add()
+    nodes = await cluster_api.nodes()
+    print(f"XXX test_2 {nodes}")
+
+
+#@pytest.mark.asyncio
+#async def test_add_node_add_column(cluster_api, random_tables):
+#    """Add a node and then add a column to a table and verify"""
+#    table = await random_tables.add_table(ncolumns=5)
+#    await cluster_api.node_add()
+#    await table.add_column()
+#    await random_tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_stop_node_add_column(cluster_api, random_tables):
+#    """Add a node, stop an original node, add a column"""
+#    nodes = await cluster_api.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await cluster_api.node_add()
+#    await cluster_api.node_stop(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_restart_node_add_column(cluster_api, random_tables):
+#    """Add a node, stop an original node, add a column"""
+#    nodes = await cluster_api.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await cluster_api.node_restart(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_remove_node_add_column(cluster_api, random_tables):
+#    """Add a node, remove an original node, add a column"""
+#    nodes = await cluster_api.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await cluster_api.node_add()
+#    await cluster_api.node_remove(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_decomission_node_add_column(cluster_api, random_tables):
+#    """Add a node, decomission an original node, add a column"""
+#    nodes = await cluster_api.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await cluster_api.node_add()
+#    await cluster_api.node_decommission(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_replace_node_add_column(cluster_api, random_tables):
+#    """Add a node, replace an original node, add a column"""
+#    nodes = await cluster_api.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await cluster_api.node_replace(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
