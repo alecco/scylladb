7a28f36988a77c2e745d7ef8efd9d00536d967c6
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-19 16:13:05 +0200

    test.py: harness for cluster management from test
    Harness for managing cluster for Python tests.
    
    Change cluster management from a list of ScyllaServer to Harness.
    Provide tests a REST API with a unix socket for control.
    
    Available commands:
        stop - stop entire cluster
        start - start entire cluster
        nodes - list cluster nodes (their server ids
        node/<id>/stop - stop node
        node/<id>/start - start node
        node/<id>/restart - restart node
        addnode - add a new node and return its id
        removenode/<id> - remove node by id
        decommission/<id> - decommission node by id
        replacenode/<id> - replace node by id, and return new node id.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/topology/test_xxx.py
@@ -0,0 +1,27 @@
+# XXX XXX XXX debug
+#
+from cassandra.protocol import InvalidRequest   # type: ignore
+from pylib.util import unique_name
+from pylib.schema_helper import get_schema
+import pytest
+import sys   # XXX
+
+async def get_nodes(harness):   # XXX
+    nodes = await harness.nodes()
+    print(f"XXX test nodes {nodes}", file=sys.stderr)  # XXX
+    return await harness.nodes()
+
+
+@pytest.mark.asyncio
+async def test_harness_xxx(cql, harness):
+    nodes = await get_nodes(harness)
+    print(f"XXX test_null removing {nodes[1]}", file=sys.stderr)  # XXX
+    await harness.node_remove(nodes[1])
+    print(f"XXX test_null removed", file=sys.stderr)  # XXX
+    await get_nodes(harness)
+    print(f"XXX test_null adding node", file=sys.stderr)  # XXX
+    ret = await harness.node_add()
+    print(f"XXX test_null added node {ret}", file=sys.stderr)  # XXX
+    nodes = await get_nodes(harness)
+    await harness.node_restart(nodes[0])
+    await get_nodes(harness)
