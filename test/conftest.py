f27b7c85d47837532ca3bdc14fb99f067797604b
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-19 16:13:05 +0200

    test.py: harness for cluster management from test
    Harness for managing cluster for Python tests.
    
    Change cluster management from a list of ScyllaServer to Harness.
    Provide tests a REST API with a unix socket for control.
    
    Add simple topology tests with a simple schema change in test_topology.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/conftest.py
+++ test/conftest.py
@@ -21,6 +21,7 @@ import pytest
 import ssl
 from typing import AsyncGenerator
 from test.pylib.schema_helper import TestTables                          # type: ignore
+from test.pylib.cluster_cli import ClusterCli
 
 
 # By default, tests run against a CQL server (Scylla or Cassandra) listening
@@ -33,6 +34,8 @@ def pytest_addoption(parser):
                      help='CQL server port to connect to')
     parser.addoption('--ssl', action='store_true',
                      help='Connect to CQL via an encrypted TLSv1.2 connection')
+    parser.addoption('--cluster_sock', action='store', required=True,
+                     help='Harness unix socket path')
 
 
 # Change default pytest-asyncio event_loop fixture scope to session to
@@ -166,10 +169,11 @@ async def fails_without_raft(request, check_pre_raft):
 # and automatically deleted at the end. We use scope="session" so that all
 # tests will reuse the same keyspace.
 @pytest.fixture(scope="session")
-async def test_keyspace(cql, this_dc):
+async def test_keyspace(cql, this_dc, cluster):
     name = unique_name()
+    replicas = await cluster.replicas()
     await cql.run_async("CREATE KEYSPACE " + name + " WITH REPLICATION = { 'class' : 'NetworkTopologyStrategy', '" +
-                this_dc + "' : 1 }")
+                this_dc + f"' : {replicas} }}")
     yield name
     await cql.run_async("DROP KEYSPACE " + name)
 
@@ -183,3 +187,10 @@ async def tables(request, cql, test_keyspace) -> AsyncGenerator:
     tables = TestTables(request.node.name, cql, test_keyspace)
     yield tables
     await tables.drop_all_tables()
+
+# "harness" fixture: set up client object for communicating with the Cluster API.
+# Connect to a Unix socket where a REST API is listening
+@pytest.fixture(scope="session")
+async def cluster(request):
+    sock_path = request.config.getoption('cluster_sock')
+    yield ClusterCli(sock_path)
