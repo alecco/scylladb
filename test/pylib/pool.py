756d3750d198bc160ef66b363eb6cec8d466d788
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-04-29 16:50:38 +0200

    squash remove Pool debug prints

--- test/pylib/pool.py
+++ test/pylib/pool.py
@@ -23,46 +23,37 @@ class Pool(Generic[T]):
 
     def __init__(self, size: int, build: Callable[[], Awaitable[T]]):
         import sys
-        print(f"XXX Pool() size {size}", file=sys.stderr)  # XXX
         assert(size >= 0)
         self.pool: asyncio.Queue[T] = asyncio.Queue(size)
         self.build = build
         self.total = 0
 
     async def get(self) -> T:
-        print(f"XXX Pool.get() empty? {self.pool.empty()} total {self.total} pool max {self.pool.maxsize}")  # XXX
         if self.pool.empty() and self.total < self.pool.maxsize:
             # Increment the total first to avoid a race
             # during self.build()
             self.total += 1
             try:
-                print(f"XXX Pool.get() self.pool.put")  # XXX
                 await self.pool.put(await self.build())
             except:     # noqa: E722
-                print(f"XXX Pool.get() total -= 1")  # XXX
                 self.total -= 1
                 raise
 
         return await self.pool.get()
 
     async def put(self, obj: T):
-        print("XXX Pool.put()")  # XXX
         await self.pool.put(obj)
 
     def instance(self) -> AsyncContextManager[T]:
-        print(f"XXX Pool.instance() type {type(self.pool)}")  # XXX
         class Instance:
             def __init__(self, pool):
-                print("XXX Pool.Instance()")  # XXX
                 self.pool = pool
 
             async def __aenter__(self):
                 self.obj = await self.pool.get()
-                print(f"XXX Pool.Instance.__aenter__() type {type(self.pool)} ret type {type(self.obj)}")  # XXX
                 return self.obj
 
             async def __aexit__(self, exc_type, exc, obj):
-                print("XXX Pool.Instance.__aexit__()")  # XXX
                 if self.obj:
                     await self.pool.put(self.obj)
                     self.obj = None
