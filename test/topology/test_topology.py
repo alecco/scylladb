1beea347c0f367fa5dbfeed3af90ac35f32461e2
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-05-17 11:40:09 +0200

    (remove) topology test keep only remove node

--- test/topology/test_topology.py
+++ test/topology/test_topology.py
@@ -19,12 +19,12 @@ async def get_nodes(harness):   # XXX
     return await harness.nodes()
 
 
-@pytest.mark.asyncio
-async def test_add_node_add_column(cql, harness):
-    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
-    await harness.node_add()
-    await tables[0].add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_add_node_add_column(cql, harness):
+#    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
+#    await harness.node_add()
+#    await tables[0].add_column()
+#    await tables.verify_schema()
 
 
 @pytest.mark.asyncio
@@ -38,43 +38,43 @@ async def test_stop_node_add_column(cql, harness):
     await tables.verify_schema()
 
 
-@pytest.mark.asyncio
-async def test_restart_node_add_column(cql, harness):
-    """Add a node, stop an original node, add a column"""
-    nodes = await harness.nodes()
-    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
-    await harness.node_restart(nodes[1])
-    await tables[0].add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_restart_node_add_column(cql, harness):
+#    """Add a node, stop an original node, add a column"""
+#    nodes = await harness.nodes()
+#    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
+#    await harness.node_restart(nodes[1])
+#    await tables[0].add_column()
+#    await tables.verify_schema()
 
 
-@pytest.mark.asyncio
-async def test_remove_node_add_column(cql, harness):
-    """Add a node, remove an original node, add a column"""
-    nodes = await harness.nodes()
-    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
-    await harness.node_add()
-    await harness.node_remove(nodes[1])
-    await tables[0].add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_remove_node_add_column(cql, harness):
+#    """Add a node, remove an original node, add a column"""
+#    nodes = await harness.nodes()
+#    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
+#    await harness.node_add()
+#    await harness.node_remove(nodes[1])
+#    await tables[0].add_column()
+#    await tables.verify_schema()
 
 
-@pytest.mark.asyncio
-async def test_decomission_node_add_column(cql, harness):
-    """Add a node, decomission an original node, add a column"""
-    nodes = await harness.nodes()
-    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
-    await harness.node_add()
-    await harness.node_decommission(nodes[1])
-    await tables[0].add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_decomission_node_add_column(cql, harness):
+#    """Add a node, decomission an original node, add a column"""
+#    nodes = await harness.nodes()
+#    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
+#    await harness.node_add()
+#    await harness.node_decommission(nodes[1])
+#    await tables[0].add_column()
+#    await tables.verify_schema()
 
 
-@pytest.mark.asyncio
-async def test_replace_node_add_column(cql, harness):
-    """Add a node, replace an original node, add a column"""
-    nodes = await harness.nodes()
-    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
-    await harness.node_replace(nodes[1])
-    await tables[0].add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_replace_node_add_column(cql, harness):
+#    """Add a node, replace an original node, add a column"""
+#    nodes = await harness.nodes()
+#    tables = await get_schema("add_node", cql, ntables=1, ncolumns=5)
+#    await harness.node_replace(nodes[1])
+#    await tables[0].add_column()
+#    await tables.verify_schema()
