164f8118464144fe7d2c4bfd58826b5868099251
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-05-30 10:52:32 +0200

    (remove) test_topology leave only stop server

--- test/topology/test_topology.py
+++ test/topology/test_topology.py
@@ -8,12 +8,12 @@ import random
 from sys import stderr
 
 
-@pytest.mark.asyncio
-async def test_add_node_add_column(cql, cluster, tables):
-    table = await tables.add_table(ncolumns=5)
-    await cluster.node_add()
-    await table.add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_add_node_add_column(cql, cluster, tables):
+#    table = await tables.add_table(ncolumns=5)
+#    await cluster.node_add()
+#    await table.add_column()
+#    await tables.verify_schema()
 
 
 @pytest.mark.asyncio
@@ -27,43 +27,43 @@ async def test_stop_node_add_column(cql, cluster, tables):
     await tables.verify_schema()
 
 
-@pytest.mark.asyncio
-async def test_restart_node_add_column(cql, cluster, tables):
-    """Add a node, stop an original node, add a column"""
-    nodes = await cluster.nodes()
-    table = await tables.add_table(ncolumns=5)
-    await cluster.node_restart(nodes[1])
-    await table.add_column()
-    await tables.verify_schema()
-
-
-@pytest.mark.asyncio
-async def test_remove_node_add_column(cql, cluster, tables):
-    """Add a node, remove an original node, add a column"""
-    nodes = await cluster.nodes()
-    table = await tables.add_table(ncolumns=5)
-    await cluster.node_add()
-    await cluster.node_remove(nodes[1])
-    await table.add_column()
-    await tables.verify_schema()
-
-
-@pytest.mark.asyncio
-async def test_decomission_node_add_column(cql, cluster, tables):
-    """Add a node, decomission an original node, add a column"""
-    nodes = await cluster.nodes()
-    table = await tables.add_table(ncolumns=5)
-    await cluster.node_add()
-    await cluster.node_decommission(nodes[1])
-    await table.add_column()
-    await tables.verify_schema()
-
-
-@pytest.mark.asyncio
-async def test_replace_node_add_column(cql, cluster, tables):
-    """Add a node, replace an original node, add a column"""
-    nodes = await cluster.nodes()
-    table = await tables.add_table(ncolumns=5)
-    await cluster.node_replace(nodes[1])
-    await table.add_column()
-    await tables.verify_schema()
+#@pytest.mark.asyncio
+#async def test_restart_node_add_column(cql, cluster, tables):
+#    """Add a node, stop an original node, add a column"""
+#    nodes = await cluster.nodes()
+#    table = await tables.add_table(ncolumns=5)
+#    await cluster.node_restart(nodes[1])
+#    await table.add_column()
+#    await tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_remove_node_add_column(cql, cluster, tables):
+#    """Add a node, remove an original node, add a column"""
+#    nodes = await cluster.nodes()
+#    table = await tables.add_table(ncolumns=5)
+#    await cluster.node_add()
+#    await cluster.node_remove(nodes[1])
+#    await table.add_column()
+#    await tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_decomission_node_add_column(cql, cluster, tables):
+#    """Add a node, decomission an original node, add a column"""
+#    nodes = await cluster.nodes()
+#    table = await tables.add_table(ncolumns=5)
+#    await cluster.node_add()
+#    await cluster.node_decommission(nodes[1])
+#    await table.add_column()
+#    await tables.verify_schema()
+#
+#
+#@pytest.mark.asyncio
+#async def test_replace_node_add_column(cql, cluster, tables):
+#    """Add a node, replace an original node, add a column"""
+#    nodes = await cluster.nodes()
+#    table = await tables.add_table(ncolumns=5)
+#    await cluster.node_replace(nodes[1])
+#    await table.add_column()
+#    await tables.verify_schema()
