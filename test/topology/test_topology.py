44dff877a6a0b4b799439048f50f052cac6ebe0f
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-07-11 16:35:36 +0200

    test.py: harness for cluster management from test
    Harness for managing cluster for Python tests.
    
    Before this patch, the same cluster was used for all test cases in a
    pytest file. This patch provides an API for tests to alter the topology
    of the cluster. When this happens, the cluster is marked dirty. Both
    cluster and driver connection are kept if the cluster is not dirty.
    
    Fixtures were changed so the harness can do before and after test
    checks and see if the cluster is dirty. The harness client helper is
    passed a function so it can make driver connections on demand as it
    makes new clusters. The cql fixture is removed as driver connection is
    managed by the harness client. The scope of keyspace fixture keyspace is
    now function as a new keyspace is needed when cluster changes.
    
    For the ScyllaCluster, split the "cluster" list of active servers into
    started (dict), stopped (dict), removed (list). The first two have host
    id as keys, and removed is just a list of host ids.
    
    Harness client helper manages all API requests to the server. For now,
    a request to create a new cluster is just waited. In the future, if a
    larger number of servers per cluster is required and would timeout the
    HTTP API request, this could be changed to return immediately and then
    the client checks until cluster is up (_wait_for_cluster()).
    
    Initial tests for topology and schema changes are added.
    
    Fixture cql_test_connection is removed as it's now redundant.
    
    API provides before/after test checks, test for harness and cluster up,
    configured cql port, list of nodes, cycle current cluster, stop/start,
    number of replicas (RF), manually mark cluster dirty. And node
    operations: add, stop, start, restart, remove, decommission, replace.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/topology/test_topology.py
@@ -0,0 +1,72 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+"""
+Test consistency of schema changes with topology changes.
+"""
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_add_node_add_column(harness, random_tables):
+    """Add a node and then add a column to a table and verify"""
+    table = await random_tables.add_table(ncolumns=5)
+    await harness.node_add()
+    await table.add_column()
+    await random_tables.verify_schema()
+
+@pytest.mark.asyncio
+async def test_stop_node_add_column(harness, random_tables):
+    """Add a node, stop an original node, add a column"""
+    nodes = await harness.nodes()
+    table = await random_tables.add_table(ncolumns=5)
+    await harness.node_add()
+    await harness.node_stop(nodes[1])
+    await table.add_column()
+    await random_tables.verify_schema()
+
+
+@pytest.mark.asyncio
+async def test_restart_node_add_column(harness, random_tables):
+    """Add a node, stop an original node, add a column"""
+    nodes = await harness.nodes()
+    table = await random_tables.add_table(ncolumns=5)
+    ret = await harness.node_restart(nodes[1])
+    print(f"XXX test_restart returned {ret}")  # XXX
+    await table.add_column()
+    await random_tables.verify_schema()
+
+
+@pytest.mark.asyncio
+async def test_remove_node_add_column(harness, random_tables):
+    """Add a node, remove an original node, add a column"""
+    nodes = await harness.nodes()
+    table = await random_tables.add_table(ncolumns=5)
+    await harness.node_add()
+    await harness.node_remove(nodes[1])
+    await table.add_column()
+    await random_tables.verify_schema()
+
+
+#@pytest.mark.asyncio
+#@pytest.mark.skip(reason="Not implemented yet")
+#async def test_decomission_node_add_column(harness, random_tables):
+#    """Add a node, decomission an original node, add a column"""
+#    nodes = await harness.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await harness.node_add()
+#    await harness.node_decommission(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
+#
+# # XXX FAILS?
+#@pytest.mark.asyncio
+#async def test_replace_node_add_column(harness, random_tables):
+#    """Add a node, replace an original node, add a column"""
+#    nodes = await harness.nodes()
+#    table = await random_tables.add_table(ncolumns=5)
+#    await harness.node_replace(nodes[1])
+#    await table.add_column()
+#    await random_tables.verify_schema()
