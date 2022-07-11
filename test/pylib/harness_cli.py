98855ed50a27609db4b673a8b27afd980f706387
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-07-11 16:35:36 +0200

    test.py: harness for cluster management from test
    Harness for managing cluster for Python tests.
    
    Before this patch, the same cluster was used for all test cases in a
    pytest file. This patch provides an API for tests to alter the topology
    of the cluster. When this happens, the cluster is marked dirty. Both
    cluster and driver connection are kept if the cluster is not dirty.
    
    Fixtures were changed so the harness can do before and after test
    checks and see if the cluster is dirty. The harness client helper is
    passed a function so it can make driver connections on demand as it
    makes new clusters. The cql fixture is removed as driver connection is
    managed by the harness client. The scope of keyspace fixture keyspace is
    now function as a new keyspace is needed when cluster changes.
    
    For the ScyllaCluster, split the "cluster" list of active servers into
    started (dict), stopped (dict), removed (list). The first two have host
    id as keys, and removed is just a list of host ids.
    
    Harness client helper manages all API requests to the server. For now,
    a request to create a new cluster is just waited. In the future, if a
    larger number of servers per cluster is required and would timeout the
    HTTP API request, this could be changed to return immediately and then
    the client checks until cluster is up (_wait_for_cluster()).
    
    Initial tests for topology and schema changes are added.
    
    Fixture cql_test_connection is removed as it's now redundant.
    
    API provides before/after test checks, test for harness and cluster up,
    configured cql port, list of nodes, cycle current cluster, stop/start,
    number of replicas (RF), manually mark cluster dirty. And node
    operations: add, stop, start, restart, remove, decommission, replace.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/pylib/harness_cli.py
@@ -0,0 +1,197 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+"""Harness client.
+   Communicates with Harness server via socket.
+   Provides helper methods to test cases.
+   Manages driver refresh when cluster is cycled.
+"""
+
+from __future__ import annotations                                         # type: ignore
+import asyncio                                                             # type: ignore
+from datetime import datetime, timedelta                                   # type: ignore
+from typing import Callable, List, Optional, TYPE_CHECKING
+import aiohttp                                                             # type: ignore
+import aiohttp.web                                                         # type: ignore
+if TYPE_CHECKING:
+    from cassandra.cluster import Session as CassandraSession              # type: ignore
+    from cassandra.cluster import Cluster as CassandraCluster              # type: ignore
+from sys import stderr  # XXX
+
+
+class HarnessOpException(Exception):
+    """Operational exception with remote API"""
+
+class HarnessTimeoutException(Exception):
+    """Harness client timed out while waiting on remote API"""
+
+class HarnessCli():
+    """Helper Harness API client
+    Args:
+        sock_path (str): path to an AF_UNIX socket where Harness server is listening
+        con_gen (Callable): generator function for CQL driver connection to a cluster
+    """
+    def __init__(self, sock_path: str, con_gen: Callable[[List[str], int], CassandraSession]):
+        self.sock_path = sock_path
+        self.conn = aiohttp.UnixConnector(path=self.sock_path)
+        self.session = aiohttp.ClientSession(connector=self.conn)
+        self.con_gen = con_gen
+        self.dirty: bool = False
+        self.cluster_nodes: List[str] = []    # List of nodes of current cluster
+        self.cluster_cql_port: int = 9042
+        self.cluster_wait_timeout: int = 30   # Wait time until cluster is up
+        self.retry_sleep: float = .1          # Retry seconds to check if cluster is up
+        self.ccluster: Optional[CassandraCluster] = None
+        self.cql: Optional[CassandraSession] = None
+
+    async def _request_and_check(self, url: str, msg: str, expect: Optional[str]=None) -> str:
+        resp = await self.session.get(url)
+        if resp.status >= 500:
+            raise HarnessOpException(msg)
+        text = await resp.text()
+        if expect is not None:
+            assert text == expect, f"Request to Harness failed, got {text}, expected {expect}"
+        return text
+
+    async def _wait_for_cluster(self) -> None:
+        """Waits until cluster is up"""
+        until = datetime.now() + timedelta(seconds=self.cluster_wait_timeout)
+        while datetime.now() < until:
+            await self._request_and_check("http://localhost/cluster/up",
+                                          "Harness server error while checking for cluster up")
+            await asyncio.sleep(self.retry_sleep)
+        raise HarnessTimeoutException("Timeout waiting for cluster to start")
+
+    def _driver_close(self) -> None:
+        if self.ccluster is not None:
+            self.ccluster.shutdown()
+            self.ccluster = None
+        self.cql = None
+
+    async def _driver_connect(self) -> None:
+        print("XXX _driver_connect() driver connecting...", file=stderr)  # XXX
+        self.ccluster = self.con_gen(await self.nodes(), await self.cql_port())
+        self.cql = self.ccluster.connect()
+        print("XXX _driver_connect() connected.", file=stderr)  # XXX
+
+    async def before_test(self, test_name: str) -> None:
+        """Before a test starts check if cluster needs cycling and update driver connection"""
+        print(f"XXX before test {test_name}", file=stderr)  # XXX
+        if await self.is_dirty():
+            print(f"   cluster dirty, closing driver", file=stderr)  # XXX
+            self._driver_close()  # Close driver connection to old cluster
+        await self._request_and_check(f"http://localhost/cluster/before_test/{test_name}",
+                                      f"Could not get a new cluster for test {test_name}")
+        await self.is_dirty()
+        if self.cql is None:
+            # TODO: if cluster is not up yet due to taking long and HTTP timeout, wait for it
+            # await self._wait_for_cluster()
+            await self._driver_connect()  # Connect driver to new cluster
+
+    async def after_test(self, test_name: str) -> None:
+        """Tell harness this test finished"""
+        print(f"XXX after test {test_name}", file=stderr)  # XXX
+        await self._request_and_check(f"http://localhost/cluster/after_test/{test_name}",
+                                      f"Could not tell harness test {test_name} ended")
+
+    async def is_up(self) -> bool:
+        """Check if Harness server is up"""
+        ret = await self._request_and_check(f"http://localhost/cluster/up",
+                                            f"Could not get if harness is up")
+        return await ret == "UP"
+
+    async def mark_dirty(self) -> None:
+        """Manually mark current cluster dirty.
+           To be used when a server was modified outside of this API."""
+        self.dirty = True
+        await self._request_and_check("http://localhost/cluster/mark-dirty",
+                                      "Could not mark cluster dirty")
+
+    async def is_dirty(self) -> bool:
+        """Check if current cluster dirty."""
+        dirty = await self._request_and_check("http://localhost/cluster/is-dirty",
+                                              "Could not check if cluster is dirty")
+        self.dirty = (dirty == "True")
+        return self.dirty
+
+    async def stop(self) -> None:
+        """Stop all active servers immediately (kill)"""
+        self.dirty = True
+        await self._request_and_check("http://localhost/cluster/stop", "Error stopping cluster")
+
+    async def stop_gracefully(self) -> None:
+        """Stop all active servers gracefully"""
+        self.dirty = True
+        await self._request_and_check("http://localhost/cluster/stop_gracefully",
+                                     "Could not stop active servers gracefully")
+
+    async def start(self) -> None:
+        """Start the cluster (if not running already)"""
+        await self._request_and_check("http://localhost/cluster/start", "Could not start cluster")
+        self.dirty = False
+
+    async def cql_port(self) -> int:
+        """Get CQL port for cluster servers"""
+        resp = await self._request_and_check("http://localhost/cluster/port",
+                                             "Error getting CQL port from cluster")
+        return int(resp)
+
+    async def replicas(self) -> int:
+        """Get number of configured replicas for the cluster (replication factor)"""
+        resp = await self._request_and_check("http://localhost/cluster/replicas",
+                                             "Error getting replicas (RF) from cluster")
+        return int(resp)
+
+    async def nodes(self) -> List[str]:
+        """Get list of running nodes"""
+        host_list = await self._request_and_check("http://localhost/cluster/nodes",
+                                                  "Error getting list of nodes")
+        self.cluster_nodes = host_list.split(',')
+        return self.cluster_nodes
+
+    async def node_stop(self, node_id: str) -> bool:
+        """Stop specified node"""
+        self.dirty = True
+        ret = await self._request_and_check(f"http://localhost/cluster/node/{node_id}/stop",
+                                            f"Error stopping server {node_id}")
+        return ret == "OK"
+
+    async def node_start(self, node_id: str) -> bool:
+        """Start specified node"""
+        self.dirty = True
+        ret = await self._request_and_check(f"http://localhost/cluster/node/{node_id}/start",
+                                            f"Error starting server {node_id}")
+        return ret == "OK"
+
+    async def node_restart(self, node_id: str) -> bool:
+        """Restart specified node"""
+        self.dirty = True
+        ret = await self._request_and_check(f"http://localhost/cluster/node/{node_id}/restart",
+                                            f"Error restarting server {node_id}")
+        return ret == "OK"
+
+    async def node_add(self) -> str:
+        """Add a new node"""
+        self.dirty = True
+        return await self._request_and_check("http://localhost/cluster/addnode",
+                                             "Error adding server")
+
+    async def node_remove(self, node_id: str) -> None:
+        """Remove a specified node"""
+        self.dirty = True
+        await self._request_and_check(f"http://localhost/cluster/removenode/{node_id}",
+                                     f"Failed to remove node {node_id}")
+
+    async def node_decommission(self, node_id: str) -> None:
+        """Decommission a specified node"""
+        self.dirty = True
+        await self._request_and_check(f"http://localhost/cluster/decommission/{node_id}",
+                                     f"Failed to decommission node {node_id}")
+
+    async def node_replace(self, node_id: str) -> None:
+        """Replace a specified node with a new one"""
+        self.dirty = True
+        await self._request_and_check(f"http://localhost/cluster/replace/{node_id}",
+                                     f"Failed to replace node {node_id}")
