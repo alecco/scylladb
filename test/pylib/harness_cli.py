e9f304d53b2f07bce0837cefd28e3745d9c11eb3
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-07-24 19:19:20 +0200

    test.py: Harness API
    Add an API via Unix socket to Harness so pytests can query information
    about the cluster. Requests are managed by HarnessCli helper class.
    
    The socket is placed inside a unique temporary directory for the
    Harness (as safe temporary socket filename is not possible).
    
    Initial API services are harness up, cluster up, if cluster is dirty,
    cql port, configured replicas (RF), and list of host ids.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/pylib/harness_cli.py
@@ -0,0 +1,73 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+"""Harness client.
+   Communicates with Harness server via socket.
+   Provides helper methods to test cases.
+   Manages driver refresh when cluster is cycled.
+"""
+
+from typing import List, Optional
+import aiohttp                                                             # type: ignore
+import aiohttp.web                                                         # type: ignore
+
+
+class HarnessOpException(Exception):
+    """Operational exception with remote API"""
+
+class HarnessCli():
+    """Helper Harness API client
+    Args:
+        sock_path (str): path to an AF_UNIX socket where Harness server is listening
+    """
+    conn: aiohttp.UnixConnector
+    session: aiohttp.ClientSession
+
+    def __init__(self, sock_path: str) -> None:
+        self.sock_path = sock_path
+
+    async def start(self):
+        """Setup connection to Harness server"""
+        self.conn = aiohttp.UnixConnector(path=self.sock_path)
+        self.session = aiohttp.ClientSession(connector=self.conn)
+
+    async def _request_and_check(self, url: str, msg: str, expect: Optional[str]=None) -> str:
+        resp = await self.session.get(url)
+        if resp.status >= 500:
+            raise HarnessOpException(msg)
+        text = await resp.text()
+        if expect is not None:
+            assert text == expect, f"Request to Harness failed, got {text}, expected {expect}"
+        return text
+
+    async def is_harness_up(self) -> bool:
+        """Check if Harness server is up"""
+        ret = await self._request_and_check("http://localhost/up",
+                                            "Could not get if harness is up")
+        return ret == "True"
+
+    async def is_cluster_up(self) -> bool:
+        """Check if cluster is up"""
+        ret = await self._request_and_check("http://localhost/cluster/up",
+                                            "Could not get if cluster is up")
+        return ret == "True"
+
+    async def is_dirty(self) -> bool:
+        """Check if current cluster dirty."""
+        dirty = await self._request_and_check("http://localhost/cluster/is-dirty",
+                                              "Could not check if cluster is dirty")
+        return dirty == "True"
+
+    async def replicas(self) -> int:
+        """Get number of configured replicas for the cluster (replication factor)"""
+        resp = await self._request_and_check("http://localhost/cluster/replicas",
+                                             "Error getting replicas (RF) from cluster")
+        return int(resp)
+
+    async def servers(self) -> List[str]:
+        """Get list of running servers"""
+        host_list = await self._request_and_check("http://localhost/cluster/servers",
+                                                  "Error getting list of servers")
+        return host_list.split(',')
