3b7bb133103fe93e67e8e4bc6756f5c2f976a08d
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-07-24 10:03:54 +0200

    test.py: Topology tests and Manager for Scylla clusters
    Preparing to cycle clusters modified (dirty) and use multiple clusters
    per topology pytest, introduce Topology tests and Manager class to
    handle clusters.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/pylib/scylla_cluster.py
+++ test/pylib/scylla_cluster.py
@@ -12,6 +12,7 @@ import shutil
 import time
 import uuid
 from typing import Optional, Dict, List, Callable
+from test.pylib.pool import Pool
 from cassandra import InvalidRequest                    # type: ignore
 from cassandra import OperationTimedOut                 # type: ignore
 from cassandra.auth import PlainTextAuthProvider        # type: ignore
@@ -484,3 +485,29 @@ class ScyllaCluster:
                                "the test must drop all keyspaces it creates.")
         for server in self.cluster:
             server.write_log_marker("------ Ending test {} ------\n".format(name))
+
+class ScyllaClusterManager:
+    """Manages a Scylla cluster for running test cases"""
+    cluster: ScyllaCluster
+
+    def __init__(self, test_name: str, clusters: Pool[ScyllaCluster]) -> None:
+        self.test_name: str = test_name
+        self.clusters: Pool[ScyllaCluster] = clusters
+
+    async def start(self) -> None:
+        """Start, start first cluster"""
+        await self._get_cluster()
+
+    async def stop(self) -> None:
+        """Stop, cycle last cluster if not dirty and present"""
+        self.cluster.after_test(self.test_name)
+        await self._return_cluster()
+
+    async def _get_cluster(self) -> None:
+        self.cluster = await self.clusters.get()
+        logging.info("Getting new Scylla cluster %s", self.cluster)
+
+    async def _return_cluster(self) -> None:
+        logging.info("Returning Scylla cluster %s", self.cluster)
+        await self.clusters.put(self.cluster)
+        del self.cluster
