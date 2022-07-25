ef50677b40b14c0fee23d9c50f596e52280000c0
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-07-25 14:02:24 +0200

    test.py: test topology and schema changes
    Add support for topology changes: add/stop/remove/restart/replace node.
    
    Test simple schema changes when changing topology.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/topology/test_topology.py
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+"""
+Test consistency of schema changes with topology changes.
+"""
+import pytest
+
+
+@pytest.mark.asyncio
+async def test_add_server_add_column(manager, random_tables):
+    """Add a node and then add a column to a table and verify"""
+    table = await random_tables.add_table(ncolumns=5)
+    await manager.server_add()
+    await table.add_column()
+    await random_tables.verify_schema()
+
+
+@pytest.mark.asyncio
+async def test_restart_server_add_column(manager, random_tables):
+    """Add a node, stop an original node, add a column"""
+    servers = await manager.servers()
+    table = await random_tables.add_table(ncolumns=5)
+    await manager.server_restart(servers[1])
+    await table.add_column()
+    await random_tables.verify_schema()
