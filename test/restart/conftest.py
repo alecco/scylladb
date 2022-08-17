aba9a7f04dd0f5fee8cf2b058eb4d1438c56bf7d
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-08-17 19:32:12 +0200

    WIP test UDA with server restart
    To test UDA after server restart add a new test suite with just 1 server
    and RF=1. For now, called "restart".
    
    Uses topology suite conftest and helpers.
    
    Not used in topology suite as those have 4 servers in config.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

---
+++ test/restart/conftest.py
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2022-present ScyllaDB
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+#
+# This file configures pytest for all tests in this directory, and also
+# defines common test fixtures for all of them to use
+
+import sys
+import pytest
+
+from test.pylib.util import unique_name
+
+
+def pytest_addoption(parser):
+    parser.addoption('--manager-api', action='store', required=True,
+                     help='Manager unix socket path')
+    parser.addoption('--ssl', action='store_true',
+        help='Connect to CQL via an encrypted TLSv1.2 connection')
+    parser.addoption('--port', action='store', default='9042',
+        help='Scylla CQL port to connect to')
+
+
+# "test_keyspace" fixture: Creates and returns a temporary keyspace to be
+# used in tests that need a keyspace. The keyspace is created with RF=1,
+# and automatically deleted at the end.
+@pytest.fixture(scope="function")
+def test_keyspace(cql):
+    name = unique_name()
+    cql.execute(f"CREATE KEYSPACE {name} WITH REPLICATION = "
+                 "{ 'class' : 'NetworkTopologyStrategy', 'replication_factor' : 1 }")
+    yield name
+    cql.execute(f"DROP KEYSPACE {name}")
+
+
+from test.topology.conftest import event_loop, manager_internal, manager, cql
+
