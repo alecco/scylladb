785f5389aab8480492e88783190d3f1020138d1f
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-09-05 10:15:00 +0200

    WIP debug  bump pytest log level and capture log

--- test.py
+++ test.py
@@ -784,7 +784,8 @@ class PythonTest(Test):
         self.is_before_test_ok = False
         self.is_after_test_ok = False
         self.args = [
-            "-s",  # don't capture print() output inside pytest
+            # XXX "-s",  # don't capture print() output inside pytest
+            "--log-level=DEBUG",
             "-o",
             "junit_family=xunit2",
             "--junit-xml={}".format(self.xmlout),
