f871743c7ce8a2c73ced60190a9d363b31661005
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-09-06 15:19:08 +0200

    test.py: whitelist for driver connections
    Since topology tests will take servers down, use a white list of know
    running servers to create new driver connections.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/topology/conftest.py
+++ test/topology/conftest.py
@@ -22,7 +22,7 @@ import cassandra                                                         # type:
 from cassandra.cluster import Session, ResponseFuture                    # type: ignore
 from cassandra.cluster import Cluster, ConsistencyLevel                  # type: ignore
 from cassandra.cluster import ExecutionProfile, EXEC_PROFILE_DEFAULT     # type: ignore
-from cassandra.policies import RoundRobinPolicy, RetryPolicy             # type: ignore
+from cassandra.policies import WhiteListRoundRobinPolicy, RetryPolicy    # type: ignore
 
 
 # Add test.pylib to the search path
@@ -123,7 +123,7 @@ def cluster_con(hosts: List[str], port: int, ssl: bool):
        It does not .connect() yet."""
     assert len(hosts) > 0, "python driver connection needs at least one host to connect to"
     profile = ExecutionProfile(
-        load_balancing_policy=RoundRobinPolicy(),
+        load_balancing_policy=WhiteListRoundRobinPolicy(hosts=hosts),
         retry_policy=TopologyChangeRetryPolicy(),
         consistency_level=ConsistencyLevel.LOCAL_QUORUM,
         serial_consistency_level=ConsistencyLevel.LOCAL_SERIAL,
