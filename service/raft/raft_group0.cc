cc06215571fc878631cfb151d948cd001e00b2db
Konstantin Osipov <kostja@scylladb.com>
2022-09-15 19:07:39 +0300

    raft: persist the list of discovered peers, not a set
    We plan to reuse the discovery table to store the peers
    after discovery is over, so load/store API must be generalized
    to use outside discovery. This includes sending
    the list of persisted peers over to a new member of the cluster.

--- service/raft/raft_group0.cc
+++ service/raft/raft_group0.cc
@@ -710,7 +710,7 @@ future<group0_peer_exchange> raft_group0::peer_exchange(discovery::peer_list pee
 
 static constexpr auto DISCOVERY_KEY = "peers";
 
-static mutation make_discovery_mutation(discovery::peer_set peers) {
+static mutation make_discovery_mutation(discovery::peer_list peers) {
     auto s = db::system_keyspace::discovery();
     auto ts = api::new_timestamp();
     auto raft_id_cdef = s->get_column_definition("raft_id");
@@ -726,23 +726,23 @@ static mutation make_discovery_mutation(discovery::peer_set peers) {
     return m;
 }
 
-static future<> store_discovered_peers(cql3::query_processor& qp, discovery::peer_set peers) {
+static future<> store_discovered_peers(cql3::query_processor& qp, discovery::peer_list peers) {
     return qp.proxy().mutate_locally({make_discovery_mutation(std::move(peers))}, tracing::trace_state_ptr{});
 }
 
-static future<discovery::peer_set> load_discovered_peers(cql3::query_processor& qp) {
+static future<discovery::peer_list> load_discovered_peers(cql3::query_processor& qp) {
     static const auto load_cql = format(
             "SELECT server_info, raft_id FROM system.{} WHERE key = '{}'",
             db::system_keyspace::DISCOVERY, DISCOVERY_KEY);
     auto rs = co_await qp.execute_internal(load_cql, cql3::query_processor::cache_internal::yes);
     assert(rs);
 
-    discovery::peer_set peers;
+    discovery::peer_list peers;
     for (auto& r: *rs) {
-        peers.emplace(
+        peers.push_back({
             raft::server_id{r.get_as<utils::UUID>("raft_id")},
             r.get_as<bytes>("server_info")
-        );
+        });
     }
 
     co_return peers;
@@ -750,10 +750,17 @@ static future<discovery::peer_set> load_discovered_peers(cql3::query_processor& 
 
 future<persistent_discovery> persistent_discovery::make(raft::server_address self, peer_list seeds, cql3::query_processor& qp) {
     auto peers = co_await load_discovered_peers(qp);
-    // If a peer is present both on disk and in provided list of `seeds`,
-    // we take the information from disk (which may already contain the Raft ID of this peer).
-    std::move(seeds.begin(), seeds.end(), std::inserter(peers, peers.end()));
-    co_return persistent_discovery{std::move(self), {peers.begin(), peers.end()}, qp};
+    // If we're restarting discovery, the loaded peer list already
+    // contains self and the old seeds. Changing the seeds between
+    // restarts is not supported, so the seeds from the
+    // configuration file should be ignored if the persisted list
+    // is not empty.
+    // discovery::step() will automatically exclude self and skip
+    // duplicates in the list.
+    if (peers.empty()) {
+        peers = std::move(seeds);
+    }
+    co_return persistent_discovery{std::move(self), peers, qp};
 }
 
 future<std::optional<discovery::peer_list>> persistent_discovery::request(peer_list peers) {
@@ -768,7 +775,7 @@ future<std::optional<discovery::peer_list>> persistent_discovery::request(peer_l
     auto holder = _gate.hold();
 
     auto response = _discovery.request(peers);
-    co_await store_discovered_peers(_qp, _discovery.peers());
+    co_await store_discovered_peers(_qp, _discovery.get_peer_list());
 
     co_return response;
 }
@@ -785,7 +792,7 @@ future<discovery::tick_output> persistent_discovery::tick() {
     // No need to enter `_gate`, since `stop` must be called after all calls to `tick` (and before the object is destroyed).
 
     auto result = _discovery.tick();
-    co_await store_discovered_peers(_qp, _discovery.peers());
+    co_await store_discovered_peers(_qp, _discovery.get_peer_list());
 
     co_return result;
 }
