5358d8d029260f60f4379a6a08a5c62e68f5dcc0
Alejo Sanchez <alejo.sanchez@scylladb.com>
2022-09-27 19:13:56 +0200

    test.py: Scylla REST methods for topology tests
    Provide a helper client for Scylla REST requests. Use it on both
    ScyllaClusterManager (e.g. remove node, test.py process) and
    ManagerClient (e.g. get uuid, pytest process).
    
    For now keep using IPs as key in ScyllaCluster, but this will be changed
    to UUID -> IP in the future. So, for now, pass both independently. Note
    the UUID must be obtained from the server before stopping it.
    
    Refresh client driver connection when removing or decommissioning a
    node.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test/pylib/manager_client.py
+++ test/pylib/manager_client.py
@@ -11,7 +11,7 @@
 
 from typing import List, Optional, Callable
 import logging
-from test.pylib.rest_client import UnixRESTClient
+from test.pylib.rest_client import UnixRESTClient, ScyllaRESTAPIClient
 from cassandra.cluster import Session as CassandraSession  # type: ignore # pylint: disable=no-name-in-module
 from cassandra.cluster import Cluster as CassandraCluster  # type: ignore # pylint: disable=no-name-in-module
 
@@ -25,6 +25,7 @@ class ManagerClient():
         sock_path (str): path to an AF_UNIX socket where Manager server is listening
         con_gen (Callable): generator function for CQL driver connection to a cluster
     """
+    # pylint: disable=too-many-public-methods
 
     def __init__(self, sock_path: str, port: int, ssl: bool,
                  con_gen: Optional[Callable[[List[str], int, bool], CassandraSession]]) -> None:
@@ -34,16 +35,18 @@ class ManagerClient():
         self.ccluster: Optional[CassandraCluster] = None
         self.cql: Optional[CassandraSession] = None
         self.cli = UnixRESTClient(sock_path)
+        self.api = ScyllaRESTAPIClient()
 
     async def stop(self):
-        """Close client session and driver"""
+        """Close api, client, and driver"""
+        await self.api.close()
         await self.cli.close()
         self.driver_close()
 
     async def driver_connect(self) -> None:
         """Connect to cluster"""
         if self.con_gen is not None:
-            servers = await self.servers()
+            servers = await self.active_servers()
             logger.debug("driver connecting to %s", servers)
             self.ccluster = self.con_gen(servers, self.port, self.ssl)
             self.cql = self.ccluster.connect()
@@ -101,9 +104,14 @@ class ManagerClient():
         resp = await self.cli.get_text("/cluster/replicas")
         return int(resp)
 
-    async def servers(self) -> List[str]:
-        """Get list of running servers"""
-        host_list = await self.cli.get_text("/cluster/servers")
+    async def running_servers(self) -> List[str]:
+        """Get list of running servers (including decommissioned)"""
+        host_list = await self.cli.get_text("/cluster/running-servers")
+        return host_list.split(",")
+
+    async def active_servers(self) -> List[str]:
+        """Get list of active servers (running and not decommissioned)"""
+        host_list = await self.cli.get_text("/cluster/active-servers")
         return host_list.split(",")
 
     async def mark_dirty(self) -> None:
@@ -140,6 +148,22 @@ class ManagerClient():
         logger.debug("ManagerClient added %s", server_id)
         return server_id
 
+    # TODO: only pass UUID
+    async def remove_node(self, initiator_ip: str, to_remove_ip: str, to_remove_uuid: str) -> None:
+        """Invoke remove node Scylla REST API for a specified server"""
+        logger.debug("ManagerClient remove node %s %s on initiator %s", to_remove_ip,
+                     to_remove_uuid, initiator_ip)
+        self.driver_close()
+        await self.cli.get_text(
+                f"/cluster/remove-node/{initiator_ip}/{to_remove_ip}/{to_remove_uuid}")
+        await self.driver_connect()
+
+    async def decommission_node(self, to_remove_ip: str) -> None:
+        """Tell a node to decommission with Scylla REST API"""
+        logger.debug("ManagerClient decommission node %s", to_remove_ip)
+        await self.cli.get_text(f"/cluster/decommission-node/{to_remove_ip}")
+        self._driver_update()
+
     async def server_get_config(self, server_id: str) -> dict[str, object]:
         resp = await self.cli.get(f"/cluster/server/{server_id}/get_config")
         if resp.status != 200:
@@ -151,3 +175,7 @@ class ManagerClient():
                                        {"key": key, "value": value})
         if resp.status != 200:
             raise Exception(await resp.text())
+
+    async def get_host_id(self, server_id: str) -> None:
+        """Get host id through Scylla REST API"""
+        return await self.api.get_host_id(server_id)
