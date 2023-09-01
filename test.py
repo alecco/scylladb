951097e3d9592cef74ceba1368030e0b4bee9793
Alejo Sanchez <alejo.sanchez@scylladb.com>
2023-09-01 12:26:20 +0200

    test.py: parallel case configuration
    Instead of specifying what test files not to parallelize, specify what
    tests to parallelize.
    
    Before this patch, all boost tests except 2 were parallelized. From
    this patch, only a known list of long-running boost tests are
    run in parallel.
    
    Long-running tests in cql-pytest and topology are now configured to be
    split to run cases independently.
    
    Signed-off-by: Alejo Sanchez <alejo.sanchez@scylladb.com>

--- test.py
+++ test.py
@@ -152,7 +152,7 @@ class TestSuite(ABC):
         self.n_failed = 0
 
         self.run_first_tests = set(cfg.get("run_first", []))
-        self.no_parallel_cases = set(cfg.get("no_parallel_cases", []))
+        self.parallel_cases = set(cfg.get("parallel_cases", []))
         # Skip tests disabled in suite.yaml
         self.disabled_tests = set(self.cfg.get("disable", []))
         # Skip tests disabled in specific mode.
@@ -402,9 +402,7 @@ class BoostTestSuite(UnitTestSuite):
         ret: List[TestCase] = []
 
         for test_name in test_list:
-            if test_name in self.no_parallel_cases:
-                case_list: List[Optional[str]] = [None]
-            else:
+            if test_name in self.parallel_cases:
                 exe = os.path.join("build", self.mode, "test", self.name, test_name)
                 if not os.access(exe, os.X_OK):
                     print(palette.warn(f"Boost test executable {exe} not found."))
@@ -415,6 +413,8 @@ class BoostTestSuite(UnitTestSuite):
                     case_list = await self._exe_list_cases(exe)
                     self._case_cache[fqname] = case_list           # store in cache
                 case_list = self._case_cache[fqname]
+            else:
+                case_list: List[Optional[str]] = [None]            # no parallel cases
 
             ret.extend([TestCase(self.name, test_name, case) for case in case_list])
 
@@ -542,15 +542,15 @@ class PythonTestSuite(TestSuite):
         return matches
 
     async def _create_test_case(self, test_name: str) -> List[TestCase]:
-        if test_name in self.no_parallel_cases:
-            case_list: List[Optional[str]] = [None]
-        else:
+        if test_name[:-3] in self.parallel_cases:
             test_file = os.path.join("test", self.name, test_name)
             if test_file not in self._case_cache:
                 case_list = await self._pytest_list_cases(test_file)
                 self._case_cache[test_file] = case_list  # Assuming cache is thread-safe
             else:
                 case_list = self._case_cache[test_file]
+        else:
+            case_list: List[Optional[str]] = [None]      # no parallel cases
 
         return [TestCase(self.name, test_name, case) for case in case_list]
 
